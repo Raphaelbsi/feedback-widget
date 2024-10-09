64af602d8c11a2e578bd35104158495804ad84bd
Raphael <oliveira_raphael@msn.com>
2024-10-09 17:18:40 -0300

    updated props

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,13 @@
 export interface FeedbackWidgetConfig {
-  integrationType: 'googleForms' | 'trello' | 'monday';
+  integrationType: 'googleForms' | 'trello' | 'monday' | 'notion';
   endpointUrl: string;
   apiKey?: string;
+  token?: string;
+  listId?: string;
+  boardId?: string;
   formId?: string;
+  entryIds?: { [key: string]: string };
+  theme?: 'light' | 'dark';
 }
 
 export const defaultConfig: FeedbackWidgetConfig = {
